c1b37905f9ad80429c35340ef7a6b11a1ffbe11f
piyush-tiwari <t.piyush@iitg.ac.in>
2020-04-22 00:25:43 +0530

    Restarting Project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,34 @@
-gitpackage com.example.myapplication;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        findViewById(R.id.textViewSignUp).setOnClickListener(this);
+    }
+
+
+
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.textViewSignUp:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+        }
+
     }
 }
